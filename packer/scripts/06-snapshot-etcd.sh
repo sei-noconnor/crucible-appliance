35a3603265833e5547a30057a475e49e1a4dc79e
Nic O'Connor <noconnor@sei.cmu.edu>
2024-08-27 21:53:22 -0400

    feature: implimented snapshot and restore makefile tasks

--- packer/scripts/06-snapshot-etcd.sh
+++ packer/scripts/06-snapshot-etcd.sh
@@ -1,2 +1,9 @@
-#!/bin/bash -e
-sudo k3s etcd-snapshot save --name crucible-appliance
+#!/bin/bash -x
+
+if [ $EUID != 0 ]; then
+    sudo "$0" "$@"
+    exit $?
+fi
+prefix="${1:-crucible-appliance}"
+
+sudo k3s etcd-snapshot save -name $prefix
