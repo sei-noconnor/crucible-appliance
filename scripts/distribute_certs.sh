2812ec34dd1b8d8224cd1ab9fd774ee184c6a507
Nic O'Connor <noconnor@sei.cmu.edu>
2024-08-27 21:53:20 -0400

    add check to make sure certs directories exists

--- scripts/distribute_certs.sh
+++ scripts/distribute_certs.sh
@@ -11,12 +11,18 @@ dst_dirs=('argocd/apps/cert-manager/kustomize/base/files')
 
 if [ $ENVIRONMENT == APPLIANCE ]; then 
     echo "Script Run on Appliance, copying"
+    if [ ! -d "$k3s_cert_dir" ]; then 
+        echo "$ADMIN_PASS" | sudo -S mkdir -p $k3s_cert_dir
+    fi
     echo "$ADMIN_PASS" | sudo -S cp -R $src_dir $k3s_cert_dir
     echo "$ADMIN_PASS" | sudo -S cp $src_dir/root-chain.pem $sdt_dir/root-chain.crt
     echo "$ADMIN_PASS" | sudo -S update-ca-certificates
 fi
 
-for i in "${dst_dirs[@]}"; do
+for dir in "${dst_dirs[@]}"; do
+    if [ ! -d $dir ]; then 
+        mkdir -p $i
+    fi 
     echo "copying certificates to $i"
     cat $src_dir/intermediate-ca.pem $src_dir/root-ca.pem > $src_dir/root-chain.pem
     cp -R $src_dir/{root*,intermediate*} $i
