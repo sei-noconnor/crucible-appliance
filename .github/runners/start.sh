9d27ede8e62bf5099ba45679785816f5e98a80cd
Nic O'Connor <nic.oconnor@gmail.com>
2024-08-27 21:32:41 -0400

    adds make tasks and scripts for github runners

--- .github/runners/start.sh
+++ .github/runners/start.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+if [[ $# -eq 0 ]]; then
+  echo "Usage: $0 <cluster_name> <cluster_ns> <github_owner> <repo_name>"
+    exit 1
+fi
 
 cluster_name=${1:-githubactions}
-ns=${2:-github}
-GITHUB_PERSONAL_TOKEN=${GITHUB_PERSON_TOKEN:-""}
+shift
+ns=${1:-github}
+shift
+gh_owner=${1}
+shift
+gh_repo=${1:-crucible-appliance-argo}
+shift
+
 echo "building the github runner image"
-docker build --platform linux/amd64 . -t github-runner:latest
+docker build --platform linux/amd64 ./.github/runners/. -t github-runner:latest
 
 echo "creating kind cluster with name $cluster_name"
 kind create cluster -n $cluster_name
@@ -12,11 +22,11 @@ echo "deploying github runner to $cluster_name on $ns namespace"
 echo "load github-runner to kind cluster $cluster_name"
 kind load docker-image github-runner:latest -n $cluster_name
 kubectl create ns $ns
-kubectl -n github create secret generic github-secret \
-  --from-literal GITHUB_OWNER=NicCOConnor \
-  --from-literal GITHUB_REPOSITORY=crucible-appliance-argo \
-  --from-literal GITHUB_PERSONAL_TOKEN="$GITHUB_PERSONAL_TOKEN"
-kubectl apply -f ./kubernetes.yaml -n github
+kubectl -n $ns create secret generic github-secret \
+  --from-literal GITHUB_OWNER=$gh_owner \
+  --from-literal GITHUB_REPOSITORY=$gh_repo \
+  --from-literal GITHUB_PERSONAL_TOKEN="$GITHUB_PERSONAL_TOKEN" 
+kubectl apply -f ./.github/runners/kubernetes.yaml -n $ns
 
 # Cleanup
 stop() {
