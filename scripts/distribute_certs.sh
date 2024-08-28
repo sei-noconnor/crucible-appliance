47f0d9ce01f99dc71b231ed311c3738adf5bb089
Nic O'Connor <noconnor@sei.cmu.edu>
2024-08-27 21:53:20 -0400

    typo distributing certs

--- scripts/distribute_certs.sh
+++ scripts/distribute_certs.sh
@@ -12,7 +12,7 @@ dst_dirs=('argocd/apps/cert-manager/kustomize/base/files')
 if [ $ENVIRONMENT == APPLIANCE ]; then 
     echo "Script Run on Appliance, copying"
     echo "$ADMIN_PASS" | sudo -S cp -R $src_dir $k3s_cert_dir
-    echo "$ADMIN_PASS" | sudo -S cp $src_dir/root-chain.pe $sdt_dir/root-chain.crt
+    echo "$ADMIN_PASS" | sudo -S cp $src_dir/root-chain.pem $sdt_dir/root-chain.crt
     echo "$ADMIN_PASS" | sudo -S update-ca-certificates
 fi
 
