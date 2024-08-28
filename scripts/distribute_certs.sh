ccb5f45779425e1f1aa43a27b842a4253d208fd2
Nic O'Connor <noconnor@sei.cmu.edu>
2024-08-27 21:53:20 -0400

    adds root certificate to k3s node trusted store

--- scripts/distribute_certs.sh
+++ scripts/distribute_certs.sh
@@ -12,11 +12,12 @@ dst_dirs=('argocd/apps/cert-manager/kustomize/base/files')
 if [ $ENVIRONMENT == APPLIANCE ]; then 
     echo "Script Run on Appliance, copying"
     echo "$ADMIN_PASS" | sudo -S cp -R $src_dir $k3s_cert_dir
+    echo "$ADMIN_PASS" | sudo -S cp $src_dir/root-chain.pe $sdt_dir/root-chain.crt
+    echo "$ADMIN_PASS" | sudo -S update-ca-certificates
 fi
 
 for i in "${dst_dirs[@]}"; do
     echo "copying certificates to $i"
     cat $src_dir/intermediate-ca.pem $src_dir/root-ca.pem > $src_dir/root-chain.pem
     cp -R $src_dir/{root*,intermediate*} $i
-    
 done
