ff7688e27c7b1dbba19c3f31695bd289dc0f51fd
Nic O'Connor <noconnor@sei.cmu.edu>
2024-10-28 17:50:08 +0000

    adds container image scripts

--- scripts/generate-images.sh
+++ scripts/generate-images.sh
@@ -1,24 +1,8 @@
-#!/bin/bash 
-echo "Not Implimented Yet!"
-exit 0;
-pvc_dir=/var/lib/rancher/k3s/storage
-certs_dir=/var/lib/rancher/k3s/server/tls 
-snapshot_dir=crucible-appliance-$(date -d)
-images=$(sudo ctr images ls | awk 'NR>1 {print $1}' | awk -v RS= '{$1=$1}1')"
-echo "$ADMIN_PASS" | sudo -S -E bash -c "k3s ctr export /tmp/appliance-snapshotappliance-images.tar $images"
-# get all images
-sudo ctr images export crucible-appliance-images.tar $(ctr images ls | awk 'NR>1 {print $1}' | awk -v RS= '{$1=$1}1')
-# get all pvcs
-pvcs=$(kubectl get pvc -A)
-# find directory with pvc id tar it up!
-### DO THAT!
-# copy certs.
-### DO THAT!
-# Take snapshot 
-sudo k3s etcd-snapshot save -name $prefix
-# Package it all up to a tar.gz
-# Include code repo in package
-# include hydration script. 
-# K6 test the cluster
+#!/bin/bash
+set -e -x
 
+cd $(dirname $0)
 
+k3s crictl images -o json \
+    | jq -r '.images[].repoTags[0] | select(. != null)' \
+    | tee image-list.txt
