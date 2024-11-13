5775dbb6d1a254ab4ca7c307ad0d38f16de22630
Nic O'Connor <noconnor@sei.cmu.edu>
2024-11-13 14:03:38 +0000

    refine longhorn values to allow for single node install - update storageClass's in values files.

--- packer/scripts/03-init-argo.sh
+++ packer/scripts/03-init-argo.sh
@@ -73,8 +73,19 @@ kubectl wait deployment \
 --for=condition=Available \
 --namespace=ingress-nginx \
 --timeout=5m
+kubectl kustomize $REPO_DEST/argocd/install/longhorn/kustomize/overlays/appliance --enable-helm | kubectl apply --wait=true -f -
+kubectl wait deployment \
+--all \
+--for=condition=Available \
+--namespace=longhorn-system \
+--timeout=5m
+# set default storageclass to longhorn remove local-path as deafult storageclass
+kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
+kubectl patch storageclass longhorn -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+
 kubectl kustomize $REPO_DEST/argocd/install/postgres/kustomize/overlays/appliance --enable-helm | kubectl apply -f -
 kubectl kustomize $REPO_DEST/argocd/install/gitea/kustomize/overlays/appliance --enable-helm | kubectl apply -f -
+kubectl kustomize $REPO_DEST/argocd/install/vault/kustomize/overlays/appliance --enable-helm | kubectl apply -f -
 kubectl kustomize $REPO_DEST/argocd/install/argocd/kustomize/overlays/appliance --enable-helm | kubectl apply -f -
 
 time=2
