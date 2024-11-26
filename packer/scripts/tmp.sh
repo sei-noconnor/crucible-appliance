d13751b49d47fdcf2be15a8807b6866800772a46
sei-noconnor <noconnor@sei.cmu.edu>
2024-11-26 18:28:52 -0500

    move keycloak to postgres in default namespace

--- packer/scripts/tmp.sh
+++ packer/scripts/tmp.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
-# Create systemd service to configure netplan primary interface
-echo "Current Directory is: ${PWD}"
-cp ./packer/scripts/configure_nic /usr/local/bin/configure_nic
-cat <<EOF > /etc/systemd/system/configure-nic.service
-[Unit]
-Description=Configure Netplan primary Ethernet interface
-After=network.target
-Before=k3s.service
-
-[Service]
-Type=oneshot
-ExecStart=/usr/local/bin/configure_nic
-
-[Install]
-WantedBy=multi-user.target
-EOF
-sudo chmod +x /usr/local/bin/configure_nic
-sudo systemctl daemon-reload
-sudo systemctl enable configure-nic
+POSTGRES_PASSWORD="odrXqetU5L"
+kubectl -n default exec postgresql-0 -- bash -c "PGPASSWORD=${POSTGRES_PASSWORD} psql -h localhost -p 5432 -U postgres -c 'CREATE EXTENSION 'uuid-ossp';" || true
+kubectl -n default exec postgresql-0 -- bash -c "PGPASSWORD=${POSTGRES_PASSWORD} psql -h localhost -p 5432 -U postgres -c "ALTER USER postgres PASSWORD 'crucible';"" || true
+DROP DATABASE alloy_db WITH (FORCE);
