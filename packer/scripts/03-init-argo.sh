c4ce5a4c154471980a273f12cf518c319a724335
Nic O'Connor <noconnor@sei.cmu.edu>
2024-11-27 14:00:17 -0500

    automate prod-argo deploy - enable steamfitter and stackstorm

--- packer/scripts/03-init-argo.sh
+++ packer/scripts/03-init-argo.sh
@@ -51,9 +51,6 @@ export ARGO_ADMIN_PASS=$(htpasswd -nbBC 10 "" "$ADMIN_PASS" | tr -d ':\n' | sed 
 #yq -i --unwrapScalar=true '.configs.secret.argocdServerAdminPassword = env(ARGO_ADMIN_PASS)' ${INSTALL_DIR}/argocd/kustomize/base/files/argocd.values.yaml
 
 # Install ArgoCD
-# kubectl kustomize ./argocd/install/argocd/kustomize/overlays/appliance --enable-helm | kubectl delete -f -
-# kubectl kustomize ./argocd/install/argocd/kustomize/overlays/appliance --enable-helm | kubectl apply -f -
-#kubectl kustomize $REPO_DEST/argocd/install/argocd/kustomize/overlays/appliance --enable-helm | kubectl delete -f -
 kubectl kustomize $REPO_DEST/argocd/install/argocd/kustomize/overlays/appliance --enable-helm | kubectl apply -f -
 
 time=2
@@ -67,31 +64,20 @@ kubectl wait deployment \
 --timeout=5m
 
 kubectl config set-context --current --namespace=argocd
-# echo "Uploading Initial Repo"
 
-# POD="$(kubectl get pods -n argocd --no-headers -l app.kubernetes.io/name=argocd-repo-server | head -n1 | awk '{print $1}')"
-# kubectl exec $POD -- bash -c "rm -rf /crucible-repo/crucible-appliance && mkdir -p /crucible-repo/crucible-appliance"
-# kubectl cp "$REPO_DEST/" "$POD:/crucible-repo/"
-# kubectl exec $POD -- bash -c "cd /crucible-repo/crucible-appliance && git config --add safe.directory '*' && git remote remove origin"
-
-# kubectl apply -f $REPO_DEST/argocd/apps/Application.yaml  
-# kubectl apply -f $APPS_DIR/cert-manager/Application.yaml
-# kubectl apply -f $APPS_DIR/nginx/Application.yaml
-# kubectl apply -f $APPS_DIR/http-echo/Application.yaml
-# kubectl apply -f $APPS_DIR/postgres/Application.yaml
-# kubectl apply -f $APPS_DIR/gitea/Application.yaml
-# kubectl apply -f $APPS_DIR/keycloak/Application.yaml
+kubectl apply -f $REPO_DEST/argocd/patches/Application.yaml
+kubectl apply -f $REPO_DEST/argocd/apps/prod-argo/root-app.yaml
 
 time=60
 echo "Sleeping $time seconds to wait for apps to sync"
 sleep $time
 
-# Wait for Deployments (most apps)
-# echo "Waiting for ALL deployments 'Status: Avaialble' This may cause a timeout."
-# kubectl wait deployment \
-# --all \
-# --for=condition=Available \
-# --all-namespaces=true \
-# --timeout=5m
+Wait for Deployments (most apps)
+echo "Waiting for ALL deployments 'Status: Avaialble' This may cause a timeout."
+kubectl wait deployment \
+--all \
+--for=condition=Available \
+--all-namespaces=true \
+--timeout=5m
 
 #rm -rf $REPO_DEST
