eb65a5415bb31ada451d29383c8347a183885c83
Administrator <Administrator@onprem.phl-imcite.net>
2025-04-17 08:18:14 -0500

    change topomojo pvc to nfs storageclass
    Generic repo-sync commit, see diff

--- packer/scripts/00-check-domain.sh
+++ packer/scripts/00-check-domain.sh
@@ -7,11 +7,11 @@ else
 fi
 
 if [ ${DOMAIN} != onprem.phl-imcite.net ]; then
-    echo "Changing domain from onprem.phl-imcite.net to ${DOMAIN}"
-    find . -type f -exec sed -i "s//${DOMAIN}/g" {} \;
+    echo "Changing domain from crucible.io to ${DOMAIN}"
+    find . -type f -exec sed -i "s/crucible.io/${DOMAIN}/g" {} \;
     echo "Changing legacy appliance domains"
-    find . -type f -exec sed -i "s/crucible.io/${DOMAIN}/g" {} \;
-    find . -type f -exec sed -i "s/crucible.io/${DOMAIN}/g" {} \;
+    find . -type f -exec sed -i "s/crucible.dev/${DOMAIN}/g" {} \;
+    find . -type f -exec sed -i "s/foundry.local/${DOMAIN}/g" {} \;
     # commit the code
     git add --all
     git commit -m " to ${DOMAIN}"    
